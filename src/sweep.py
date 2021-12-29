6113f9b2bc06c1e192b730cea2a98e140bb0f24f
Isabella <82531496+isabelladegen@users.noreply.github.com>
2021-12-29 15:59:30 +0000

    bayes sweep with 10% of the training data

--- src/sweep.py
+++ src/sweep.py
@@ -1,8 +1,9 @@
 import wandb
 from tune_model_parameters_experiment import train
+from src.evaluate import SCORE_F1
 
 if __name__ == '__main__':
-    sweep_config = {
+    sweep_config_grid = {
         'name': 'test',
         'method': 'grid',
         'parameters': {
@@ -18,5 +19,31 @@ if __name__ == '__main__':
         }
     }
 
-    sweep_id = wandb.sweep(sweep_config, project="test")
+    sweep_config_bayes = {
+        'name': 'test',
+        'method': 'bayes',
+        'metric': {
+            'goal': 'maximise',
+            'name': SCORE_F1
+        },
+        'parameters': {
+            'vector_size': {
+                'values': [20, 30, 70, 80, 100]
+            },
+            'epochs': {
+                'values': [40, 50, 100, 150, 200]
+            },
+            'dm': {
+                'values': [0, 1]
+            },
+        },
+        'early_terminate': {
+            'type': 'hyperband',
+            's': 2,
+            'eta': 3,
+            'max_iter': 27
+        }
+    }
+
+    sweep_id = wandb.sweep(sweep_config_bayes, project="test")
     wandb.agent(sweep_id, function=train)
